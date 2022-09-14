140bddc0e8df58072486dcee500b9ff8080575ee
spartakgubarev <spartakgubarev@rambler.ru>
2022-09-14 23:30:20 +0300

    add Seminare03_ClassRoom 02

--- Seminar03/Seminar03.py
+++ Seminar03/Seminar03.py
@@ -1,32 +1,46 @@
 # 1. Реализуйте алгоритм задания случайных чисел без использования встроенного генератора псевдослучайных чисел.
-import time
-def rnd():
-    str_list = input(
-        'Введите диапозон случайных чисел через пробел, пример: [1 10]: ').split(sep=' ')
-    int_list = [int(x) for x in str_list]
-    # str_list = ['50', '1000']
-    # int_list = [50, 1000]
-    b = True
-    find = ''
-    while b:
-        if len(find) > len(str(1)):
-            find = ''
-        sec = time.time()
-        time.sleep(1/int(str(sec)[-4:]))
-        find += str(sec)[-2]             # предпоследний символ чтобы нуль включался тоже, иначе ноль обрезается
-        if int(str(sec)[-1]) > 5:
-            find += str(sec)[-2]
-        if int_list[0] < int(find) < int_list[1]:
-            b = False
-    return find
-rnd_rnd = int(rnd())
-print(rnd_rnd)
+# import time
+# def rnd():
+#     str_list = input(
+#         'Введите диапозон случайных чисел через пробел, пример: [1 10]: ').split(sep=' ')
+#     int_list = [int(x) for x in str_list]
+#     # str_list = ['50', '1000']
+#     # int_list = [50, 1000]
+#     b = True
+#     find = ''
+#     while b:
+#         if len(find) > len(str(1)):
+#             find = ''
+#         sec = time.time()
+#         time.sleep(1/int(str(sec)[-4:]))
+#         find += str(sec)[-2]             # предпоследний символ чтобы нуль включался тоже, иначе ноль обрезается
+#         if int(str(sec)[-1]) > 5:
+#             find += str(sec)[-2]
+#         if int_list[0] < int(find) < int_list[1]:
+#             b = False
+#     return find
+# rnd_rnd = int(rnd())
+# print(rnd_rnd)
 
- 
+
 # 2. Задайте список. Напишите программу, которая определит, присутствует ли в заданном списке строк некое число.
 # ['efg23'. '79234gefg', 'sdgs', 'g53']
 # '2
 # ['efg23', '79234gefg']
+arr = ['ertg45yfg23', '792345yt45y4gefg', 'sdfh54h54ygs',
+       'g545y4yy45y3', '34545ttgr', '56544reht55', '45yh65jreh']
+arr_new = []
+index = 0
+number = str(input('Введите искомое число: '))
+for i in arr:
+    count = 0
+    while count < len(i):
+        if number == i[count:count + len(number)]:
+            arr_new.append(i)
+            count = len(i)
+        count += 1
+print(arr_new)
+
 
 # 3. Напишите программу, которая определит позицию второго вхождения строки в списке, либо сообщит, что ее нет.
 # Пример:
