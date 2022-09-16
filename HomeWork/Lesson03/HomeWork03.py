f9b803710b2c4627b60d78f16df1c1534e177c92
spartakgubarev <spartakgubarev@rambler.ru>
2022-09-16 18:38:54 +0300

    add Task 02

--- HomeWork/Lesson03/HomeWork03.py
+++ HomeWork/Lesson03/HomeWork03.py
@@ -3,20 +3,33 @@
 # 1. Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка,
 # стоящих на нечётной позиции.
 # Пример: # - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
-def sum_odd_numbers(arr):
-    summ = 0
-    for i in range(1, len(arr), 2):
-        summ += arr[i]
-    return summ
-
-
-list1 = [2, 3, 5, 9, 3, 12, 17, 3, 2]
-print(sum_odd_numbers(list1))
+# def sum_odd_numbers(arr):
+#     summ = 0
+#     for i in range(1, len(arr), 2):
+#         summ += arr[i]
+#     return summ
+# list1 = [2, 3, 5, 9, 3, 12, 17, 3, 2]
+# print(sum_odd_numbers(list1))
 
 # 2. Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый
 # и последний элемент, второй и предпоследний и т.д.
 # Пример: # - [2, 3, 4, 5, 6] => [12, 15, 16];
 # - [2, 3, 5, 6] => [12, 15]
+def multiplication(arr):
+    list_result = []
+    parity_check = len(arr)
+    if parity_check % 2 == 1:
+        parity_check = parity_check // 2 + 1
+    else:
+        parity_check //= 2
+    for i in range(parity_check):
+        list_result.append(arr[i] * arr[-i-1])
+    return list_result
+
+
+list = [1, 2, 3, 4, 5, 6, 7]
+print(list)
+print(multiplication(list))
 
 
 # 3. Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу между
