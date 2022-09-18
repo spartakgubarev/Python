287a60838b4353a942f5b08bbe7b4f9be45296ab
spartakgubarev <spartakgubarev@rambler.ru>
2022-09-18 23:20:48 +0300

    add Task 05.1

--- HomeWork/Lesson04/HomeWork04.py
+++ HomeWork/Lesson04/HomeWork04.py
@@ -56,36 +56,60 @@
 # Пример:
 # k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 
-import random
+# import random
 
 
-def degree_polynomial(count, rnd_min, rnd_max):
-    k = count
-    func = 'k = ' + str(count) + ' --> '
-    k_str = ''
-    while count != 0:
-        rnd = (random.randint(rnd_min, rnd_max))
-        k_str = str(count)
-        func += str(rnd) + '*' + 'x^' + k_str + ' + '
-        if count == 1:
-            rnd = (random.randint(rnd_min, rnd_max))
-            func += str(rnd) + ' = 0'
-        count -= 1
-    return func
+# def degree_polynomial(count, rnd_min, rnd_max):
+#     k = count
+#     func = 'k = ' + str(count) + ' --> '
+#     k_str = ''
+#     while count != 0:
+#         rnd = (random.randint(rnd_min, rnd_max))
+#         k_str = str(count)
+#         func += str(rnd) + '*' + 'x^' + k_str + ' + '
+#         if count == 1:
+#             rnd = (random.randint(rnd_min, rnd_max))
+#             func += str(rnd) + ' = 0'
+#         count -= 1
+#     return func
 
 
-rnd_min1 = 0
-rnd_max2 = 100
-count1 = int(input('Введите натуральную степень числа к= '))
-formula = degree_polynomial(count1, rnd_min1, rnd_max2)
-print(f'{formula}')
+# rnd_min1 = 0
+# rnd_max2 = 100
+# count1 = int(input('Введите натуральную степень числа к= '))
+# formula = degree_polynomial(count1, rnd_min1, rnd_max2)
+# print(f'{formula}')
 
-path = 'G:/Учеба/Разработчик/repo/Python/HomeWork/Lesson04/degree_polynomial.txt'
-with open(path, 'a') as data:
-    data.write(formula + '\n')
-    data.close()
+# path = 'G:/Учеба/Разработчик/repo/Python/HomeWork/Lesson04/degree_polynomial.txt'
+# with open(path, 'a') as data:
+#     data.write(formula + '\n')
+#     data.close()
 
 
 # ******
 # 5. Даны два файла, в каждом из которых находится запись многочлена.
 # Задача - сформировать файл, содержащий сумму многочленов.
+
+# Чтение данных из файла
+def data_extraction(path_end):
+    data = open(path_end, 'r')
+    for line in data:
+        text_end = line
+    data.close
+    return text_end
+
+path = 'G:/Учеба/Разработчик/repo/Python/HomeWork/Lesson04/degree_polynomial.txt'
+path1 = 'G:/Учеба/Разработчик/repo/Python/HomeWork/Lesson04/degree_polynomial_1.txt'
+text = data_extraction(path)
+text1 = data_extraction(path1)
+
+print(text)
+print(text1)
+
+# Чтение данных из файла
+# path = 'file.txt'
+# data = open(path, 'r')
+# for line in data:
+#     print(line)
+# data.close
+# exit()      # после этой команды код не будет выполнять никакой!!!
