71a89f64fbc33a653f54df03e89808871bd1f63c
spartakgubarev <spartakgubarev@rambler.ru>
2022-09-18 20:04:03 +0300

    add Task 03

--- HomeWork/Lesson04/HomeWork04.py
+++ HomeWork/Lesson04/HomeWork04.py
@@ -20,23 +20,35 @@
 # ******
 # 2. Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
 # "20" -> [2, 2, 5]
-def prime_factors(n):
-    list = []
-    for i in range(2, n+1):
-        while n % i == 0:
-            n = n / i
-            list.append(i)
-    return list
+# def prime_factors(n):
+#     list = []
+#     for i in range(2, n+1):
+#         while n % i == 0:
+#             n = n / i
+#             list.append(i)
+#     return list
 
 
-number = int(input('Введите число, составлю простые множители числа: '))
-list = prime_factors(number)
-print(list)
+# number = int(input('Введите число, составлю простые множители числа: '))
+# list = prime_factors(number)
+# print(list)
 
 # ******
 # 3. Задайте последовательность чисел. Напишите программу, которая выведет список
 # неповторяющихся элементов исходной последовательности.
 # [1, 1, 2, 3, 4, 5, 5] -> [2, 3, 4]
+def search_repetitions(old_list):
+    _list = []
+    for i in range(len(old_list)):
+        if (old_list[i] not in old_list[i+1:]) and (old_list[i] not in old_list[:i]):
+            _list.append(old_list[i])
+    return _list
+
+
+new_list = [8, 7, 6, 1, 2, 4, 1, 5, 6, 7, 9, 8, 1, 3, 4]
+list = search_repetitions(new_list)
+print(f'{new_list} -> {list}')
+
 
 # ******
 # 4. Задана натуральная степень k. Сформировать случайным образом список коэффициентов
