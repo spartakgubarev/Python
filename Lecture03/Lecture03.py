0b4e8bb7fd4b914f06098d1c5541b0402b871cce
spartakgubarev <spartakgubarev@rambler.ru>
2022-09-21 17:30:28 +0300

    add  function enumerate 0.2

--- Lecture03/Lecture03.py
+++ Lecture03/Lecture03.py
@@ -128,3 +128,9 @@
 
 
 # enumerate ******************
+users = ['user1', 'user2', 'user3', 'user4', 'user5']
+ids = [4, 5, 9, 14, 7]
+salary = [111, 222, 333]
+
+data = list(enumerate(users))
+print(data)
