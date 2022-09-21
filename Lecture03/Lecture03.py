d3268d6fad2f5e396dd8aeca09c03b772b2eafa6
spartakgubarev <spartakgubarev@rambler.ru>
2022-09-21 17:00:30 +0300

    add map

--- Lecture03/Lecture03.py
+++ Lecture03/Lecture03.py
@@ -33,4 +33,67 @@
 #     list.append(i)
 # print(list)
 
-list = [i for i in range(1, 21)]
+# def f(x):
+#     return x**3
+
+# list = [f(i) for i in range(1, 21) if i % 2 == 0] # списки
+# print(list)
+
+# list = [(i, f(i))  for i in range(1, 21) if i % 2 == 0] # кортежи
+# print(list)
+
+# ***************
+# Есть список 1 2 3 5 8 15 23 38, Получить: # [(2, 4), (8, 64), (38, 144)]
+# path = 'G:/Учеба/Разработчик/repo/Python/Lecture03/text.txt'
+# f = open(path, 'r')
+# data = f.read() + ' '
+# print(data)
+# f.close()
+
+# numbers = []
+
+# while data != '':
+#     space_pos = data.index(' ')
+#     numbers.append(int(data[:space_pos]))
+#     data = data[space_pos +1:]
+
+# out = []
+# for e in numbers:
+#     if not e % 2:
+#         out.append((e, e **2))
+# print(out)
+
+# ***************
+# sum = lambda x: x**2
+# list = [1, 2, 3, 5, 8, 15, 23, 38]
+# list_sum = [(i,sum(i)) for i in list if i%2 == 0]
+# print(list_sum)
+
+
+# ***************
+# def select(f, col):
+#     return [f(x) for x in col]
+
+# def where(f, col):
+#     return [x for x in col if f(x)]
+
+# data = '1 2 3 5 8 15 23 38'.split()
+
+# res = select(int, data)
+# res = where(lambda x: not  x % 2, res)
+# res = select(lambda x: (x, x**2), res)
+# print(res)
+
+
+# map ******************** map
+# li = [x for x in range(1, 21)]
+# li = list(map(lambda x: (x, x+10), li))
+# print(li)
+
+# data = list(map(int, input().split()))
+# print(data)
+
+
+data = map(int, '1 2 3 23 12 12'.split())
+for e in data: 
+    print(data)
