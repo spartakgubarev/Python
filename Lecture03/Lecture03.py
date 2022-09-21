c6af0358e294b5f41f260c23b985214370069e88
spartakgubarev <spartakgubarev@rambler.ru>
2022-09-21 17:26:19 +0300

    add  function zip 0.2

--- Lecture03/Lecture03.py
+++ Lecture03/Lecture03.py
@@ -120,3 +120,9 @@
 
 # zip ******************
 
+users = ['user1', 'user2', 'user3', 'user4', 'user5']
+ids = [4, 5, 9, 14, 7]
+salary = [111, 222, 333]
+
+data = list(zip(users, ids, salary))
+print(data)
