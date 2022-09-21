ef973bff6c8e1571640ff8a193afb1348374cd1a
spartakgubarev <spartakgubarev@rambler.ru>
2022-09-21 17:20:21 +0300

    add  function zip 0.1

--- Lecture03/Lecture03.py
+++ Lecture03/Lecture03.py
@@ -111,9 +111,12 @@
 
 
 # filter **************
-data = '1 2 3 5 8 15 23 38'.split()
-res = map(int, data)
-res = filter(lambda x: not  x % 2, res)
-res = list(map(lambda x: (x, x**2), res))
-print(res)
+# data = '1 2 3 5 8 15 23 38'.split()
+# res = map(int, data)
+# res = filter(lambda x: not  x % 2, res)
+# res = list(map(lambda x: (x, x**2), res))
+# print(res)
+
+
+# zip ******************
 
