4810a143248c5446945321ae840ebf2de32910ad
spartakgubarev <spartakgubarev@rambler.ru>
2022-09-21 17:17:25 +0300

    add  function filter 0.1

--- Lecture03/Lecture03.py
+++ Lecture03/Lecture03.py
@@ -100,11 +100,20 @@
 #     print(e*10)
 
 
-def where(f, col):
-    return [x for x in col if f(x)]
+# def where(f, col):
+#     return [x for x in col if f(x)]
 
+# data = '1 2 3 5 8 15 23 38'.split()
+# res = map(int, data)
+# res = where(lambda x: not  x % 2, res)
+# res = list(map(lambda x: (x, x**2), res))
+# print(res)
+
+
+# filter **************
 data = '1 2 3 5 8 15 23 38'.split()
 res = map(int, data)
-res = where(lambda x: not  x % 2, res)
+res = filter(lambda x: not  x % 2, res)
 res = list(map(lambda x: (x, x**2), res))
 print(res)
+
