89f1c3531fcc843232a42370f457015187ea9859
spartakgubarev <spartakgubarev@rambler.ru>
2022-09-21 17:27:19 +0300

    add  function enumerate 0.1

--- Lecture03/Lecture03.py
+++ Lecture03/Lecture03.py
@@ -119,10 +119,12 @@
 
 
 # zip ******************
+# users = ['user1', 'user2', 'user3', 'user4', 'user5']
+# ids = [4, 5, 9, 14, 7]
+# salary = [111, 222, 333]
 
-users = ['user1', 'user2', 'user3', 'user4', 'user5']
-ids = [4, 5, 9, 14, 7]
-salary = [111, 222, 333]
+# data = list(zip(users, ids, salary))
+# print(data)
 
-data = list(zip(users, ids, salary))
-print(data)
+
+# enumerate ******************
