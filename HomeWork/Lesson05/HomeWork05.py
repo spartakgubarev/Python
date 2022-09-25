6a05f66aab7776e8640d85a5414343f82533d405
spartakgubarev <spartakgubarev@rambler.ru>
2022-09-25 21:06:50 +0300

    add HomeWork Task 03.0

--- HomeWork/Lesson05/HomeWork05.py
+++ HomeWork/Lesson05/HomeWork05.py
@@ -25,92 +25,94 @@
 # Сколько конфет нужно взять первому игроку, чтобы забрать все конфеты у своего конкурента?
 #     а) Добавьте игру против бота
 #     б) подумайте как наделить бота "интеллектом"
-import random
+# import random
 
 
-def number_check():
-    while True:
-        num = input()
-        if num.isdigit():
-            num = int(num)
-            if num >= 1 and num <= 28:
-                return num
-            else:
-                print('Внимательнее, от 1 по 28')
-                False
-        else:
-            print('Внимательнее, Вы ввели не число.')
-            False
+# def number_check():
+#     while True:
+#         num = input()
+#         if num.isdigit():
+#             num = int(num)
+#             if num >= 1 and num <= 28:
+#                 return num
+#             else:
+#                 print('Внимательнее, от 1 по 28')
+#                 False
+#         else:
+#             print('Внимательнее, Вы ввели не число.')
+#             False
 
 
-def mode_player():
-    list = []
-    num = int(input('Введите вариант игры: '))
-    player1 = input('Введите имя игрока: ')
-    if num == 1:
-        player2 = input('Введите имя второго игрока: ')
-    elif num == 2:
-        player2 = 'computer'
-    elif num == 3:
-        player2 = 'computer_expert'
-    rnd = random.randint(1, 2)
-    if rnd == 1:
-        list = [player1, player2, num]
-        return list
-    else:
-        list = [player2, player1, num]
-        return list
+# def mode_player():
+#     list = []
+#     num = int(input('Введите вариант игры: '))
+#     player1 = input('Введите имя игрока: ')
+#     if num == 1:
+#         player2 = input('Введите имя второго игрока: ')
+#     elif num == 2:
+#         player2 = 'computer'
+#     elif num == 3:
+#         player2 = 'computer_expert'
+#     rnd = random.randint(1, 2)
+#     if rnd == 1:
+#         list = [player1, player2, num]
+#         return list
+#     else:
+#         list = [player2, player1, num]
+#         return list
 
 
-def choice_move(mode):
-    if mode[2] == 1:
-        number = number_check()
-    elif mode[2] == 2:
-        if mode[0] != 'computer':
-            number = number_check()
-        else:
-            if count < 29:
-                number = count
-            else:
-                number = random.randint(1, 28)
-            print(number)
-    elif mode[2] == 3:
-        if mode[0] != 'computer_expert':
-            number = number_check()
-        else:
-            number = expert(count)
-            print(number)
-    return number
+# def choice_move(mode):
+#     if mode[2] == 1:
+#         number = number_check()
+#     elif mode[2] == 2:
+#         if mode[0] != 'computer':
+#             number = number_check()
+#         else:
+#             if count < 29:
+#                 number = count
+#             else:
+#                 number = random.randint(1, 28)
+#             print(number)
+#     elif mode[2] == 3:
+#         if mode[0] != 'computer_expert':
+#             number = number_check()
+#         else:
+#             number = expert(count)
+#             print(number)
+#     return number
 
 
-def expert(count):
-    if 0 < (count % 29) < 29:
-        _temp = count % 29
-        return _temp
-    _temp = random.randint(1, 28)
-    return _temp
+# def expert(count):
+#     if 0 < (count % 29) < 29:
+#         _temp = count % 29
+#         return _temp
+#     _temp = random.randint(1, 28)
+#     return _temp
 
 
-count = 2021
+# count = 2021
 
-print(
-    f'На столе лежит {count} конфет(а). Играют двое. Очередность определяется случайно.')
-print('За один ход можно взять от 1-28 конфет. Выиграл тот, кто сделал последний ход.')
-print('Как играем?\n1 - Человек-человек\n2 - Человек-компьютер\n3 - Человек-копьютер(профи)')
+# print(
+#     f'На столе лежит {count} конфет(а). Играют двое. Очередность определяется случайно.')
+# print('За один ход можно взять от 1-28 конфет. Выиграл тот, кто сделал последний ход.')
+# print('Как играем?\n1 - Человек-человек\n2 - Человек-компьютер\n3 - Человек-копьютер(профи)')
 
-mode = mode_player()
+# mode = mode_player()
 
-while count > 0:
-    print(f'Осталось {count} конфет. Ходит {mode[0]}, сколько забираете: ')
-    number = choice_move(mode)
-    temp = mode[0]
-    mode[0] = mode[1]
-    mode[1] = temp
-    count -= number
-print(f'Выиграл {mode[1]}. Поздравляем!!!')
+# while count > 0:
+#     print(f'Осталось {count} конфет. Ходит {mode[0]}, сколько забираете: ')
+#     number = choice_move(mode)
+#     temp = mode[0]
+#     mode[0] = mode[1]
+#     mode[1] = temp
+#     count -= number
+# print(f'Выиграл {mode[1]}. Поздравляем!!!')
 
 
 # ********************* 3. Создайте программу для игры в "Крестики-нолики".
+
+
 
 
 # ********************* 4. Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
