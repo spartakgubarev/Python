af0db2fc23ddf924afe271cc301d6672be1c8dd3
spartakgubarev <spartakgubarev@rambler.ru>
2022-10-03 19:44:44 +0300

    add HomeWork Task 04.0 and zip_zip.txt

--- HomeWork/Lesson05/HomeWork05.py
+++ HomeWork/Lesson05/HomeWork05.py
@@ -111,50 +111,51 @@
 
 
 # ********************* 3. Создайте программу для игры в "Крестики-нолики".
-from tkinter import *
+# from tkinter import *
 
 
-def clicked(btn1, x, y):
-    global count
-    btn1["state"] = DISABLED
-    if count % 2 == 0:
-        btn1['text'] = 'X'
-    else:
-        btn1['text'] = 'O'
-    btn1["state"] = DISABLED
-    count += 1
-    a = btn1['text']
-    btn[x][y] = a
+# def clicked(btn1, x, y):
+#     global count
+#     btn1["state"] = DISABLED
+#     if count % 2 == 0:
+#         btn1['text'] = 'X'
+#     else:
+#         btn1['text'] = 'O'
+#     btn1["state"] = DISABLED
+#     count += 1
+#     a = btn1['text']
+#     btn[x][y] = a
 
-    if btn[0][0] == a and btn[0][1] == a and btn[0][2] == a or (
-            btn[1][0] == a and btn[1][1] == a and btn[1][2] == a or
-            btn[2][0] == a and btn[2][1] == a and btn[2][2] == a or
-            btn[0][0] == a and btn[1][0] == a and btn[2][0] == a or
-            btn[0][1] == a and btn[1][1] == a and btn[2][1] == a or
-            btn[0][2] == a and btn[1][2] == a and btn[2][2] == a or
-            btn[0][0] == a and btn[1][1] == a and btn[2][2] == a or
-            btn[0][2] == a and btn[1][1] == a and btn[2][0] == a):
-        print(f'Победили "{a}", УРААААА!!!!')
-        for i in range(3):
-            for j in range(3):
-                if btn[i][j] != 'X' and btn[i][j] != 'O':
-                    btn[i][j]['state'] = DISABLED
+#     if btn[0][0] == a and btn[0][1] == a and btn[0][2] == a or (
+#             btn[1][0] == a and btn[1][1] == a and btn[1][2] == a or
+#             btn[2][0] == a and btn[2][1] == a and btn[2][2] == a or
+#             btn[0][0] == a and btn[1][0] == a and btn[2][0] == a or
+#             btn[0][1] == a and btn[1][1] == a and btn[2][1] == a or
+#             btn[0][2] == a and btn[1][2] == a and btn[2][2] == a or
+#             btn[0][0] == a and btn[1][1] == a and btn[2][2] == a or
+#             btn[0][2] == a and btn[1][1] == a and btn[2][0] == a):
+#         l1 = Label(text=f'Победили "{a}", УРААААА!!!!')
+#         l1.grid(column=4, row=4)
+#         for i in range(3):
+#             for j in range(3):
+#                 if btn[i][j] != 'X' and btn[i][j] != 'O':
+#                     btn[i][j]['state'] = DISABLED
 
 
-btn = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-count = 0
-window = Tk()
-window.title('"крестики-нолики"')
-window.geometry('400x250')
+# btn = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+# count = 0
+# window = Tk()
+# window.title('"крестики-нолики"')
+# window.geometry('400x250')
 
-for i in range(3):
-    for j in range(3):
-        btn[i][j] = Button(window, text='', command=lambda x=i,
-                           y=j: clicked(btn[x][y], x, y), width=4)
-        btn[i][j].grid(column=j, row=i)
-
-window.mainloop()
+# for i in range(3):
+#     for j in range(3):
+#         btn[i][j] = Button(window, text='', command=lambda x=i,
+#                            y=j: clicked(btn[x][y], x, y), width=4)
+#         btn[i][j].grid(column=j, row=i)
+# window.mainloop()
 
 
 # ********************* 4. Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
 # Входные и выходные данные хранятся в отдельных текстовых файлах.
+# aaaffffeee -> a3f4e3
