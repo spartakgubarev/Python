3e42e3903695935350d3f4e44fb68eaa741d8d1e
spartakgubarev <spartakgubarev@rambler.ru>
2022-10-09 17:54:52 +0300

    add project01 rand_text

---
+++ Lecture04/Homework 01/rand_text.py
@@ -0,0 +1,14 @@
+from datetime import datetime as dt
+from time import time
+
+
+amount = 1000
+time = dt.now().strftime('%D:%H:%M -')
+path = 'G:/Учеба/Разработчик/repo/Python/Lecture04/Homework 01/text_client.txt'
+file = open(path, 'w')
+
+for i in range(amount):
+    file.write(f'{time} id{i} name{i} surname{i} birth{i} place_of_work{i} phone_number{i}\n')
+file.close()
+    # with open(path, 'w') as file:
+    #     file.write(f'{time} id{i} name{i} surname{i} birth{i} place_of_work{i} phone_number{i}\n')    
