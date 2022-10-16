9344f28b32eb5d3ef9a483bf47ca45a04b8ba8bd
spartakgubarev <spartakgubarev@rambler.ru>
2022-10-16 12:06:47 +0300

    calc 0.1

--- Seminar06/controller.py
+++ Seminar06/controller.py
@@ -2,7 +2,8 @@ import procedure as proc
 
 
 def button_click():
-    text = input('Введите выражение в формате: "5+4-3/2*6-7": ')
+    # text = input('Введите выражение в формате: "5+4-3/2*6-7": ')
+    text = '20/1+3/1-5*7-40-3/2-10/1'
     text += "="
     text_string = proc.procedure(text)
     text_string = float(text_string[:-1])
--- Seminar06/search.py
+++ Seminar06/search.py
@@ -2,16 +2,23 @@ import result as rs
 
 
 def search_values(text, sign):
-    find_index = text.find(sign)
-    i = find_index
+    if text[0] == '-':
+        find_index = text[1:].find(sign) + 1
+        i = find_index
+    else:
+        find_index = text.find(sign)
+        i = find_index
     min_index = 0
     max_index = 0
 
-    while i > 0 and text[i-1] >= '0' and text[i-1] <= '9' or text[i-1] == '.':
+    while i > 0 and text[i-1] >= '0' and text[i-1] <= '9' or text[i-1] == '.' or text[i-1] == text[0]:
         i -= 1
     min_index = i
-    i = find_index
-    
+    if text[0] == '-':
+        i = find_index + 1
+    else:
+        i = find_index
+
     while i+1 < len(text)-1 and text[i+1] >= '0' and text[i+1] <= '9' or text[i+1] == '.':
         i += 1
     max_index = i
@@ -19,5 +26,6 @@ def search_values(text, sign):
     first_value = float(text[min_index:find_index])
     second_value = float(text[find_index+1:max_index+1])
     res = rs.result(first_value, second_value, sign)
-    text = text.replace(text[min_index:max_index+1], str(res))
+
+    text = text.replace(text[min_index:max_index+1], str(res), 1)
     return text
