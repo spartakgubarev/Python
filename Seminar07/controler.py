a723101600937eb7925a19cb171ef2cdda8e4b1f
spartakgubarev <spartakgubarev@rambler.ru>
2022-11-11 21:21:33 +0300

    Seminar07 add map

--- Seminar07/controler.py
+++ Seminar07/controler.py
@@ -6,6 +6,6 @@ def calculation():
     operation = view.operation_request()
 
     db.saving_number(number_1, number_2, operation)
-    number = [int(i) for i in db.reading_number().split(f' {operation} ')]
+    number = list(map(int, db.reading_number().split(f' {operation} ')))
     result = math_f.equation(operation, number[0], number[1])
     view.output(number[0], number[1], operation, result)
