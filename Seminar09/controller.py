ffdad12ea1511d9193168c2114cee7a04be9239d
spartakgubarev <spartakgubarev@rambler.ru>
2022-11-18 21:21:02 +0300

    Seminar09

---
+++ Seminar09/controller.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+
+def main():
+    window = Tk()
+    window.title("Приложение для скачивания с YouTube")
+    lbl = Label(window, text="Привет")
+
+
+
+
+
+    window.mainloop()
