3bc622dad52b59d4924ec5e5210cf0605f6decf7
spartakgubarev <spartakgubarev@rambler.ru>
2023-01-29 11:23:24 +0300

    Classwork Lecture05

---
+++ Lecture05/New/main.py
@@ -0,0 +1,19 @@
+# from progress.bar import Bar
+# import time
+# import emoji
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+ls = [1,2,3,2,7]
+plt.plot(ls)
+plt.show()
+# bar = Bar('Выполняется', max=10)
+# for i in range(100):
+#     time.sleep(1)
+#     # Do some work
+#     bar.next()
+# bar.finish()
+
+
+# print(emoji.emojize('Python is :baby_angel:'))
