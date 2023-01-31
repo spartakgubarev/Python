80bbb8b2bbea91c01c94ad7c33fccdff82f617b4
spartakgubarev <spartakgubarev@rambler.ru>
2023-01-31 17:45:36 +0300

    log1.csv ignore

--- HomeWork/Lesson09/new/spy.py
+++ HomeWork/Lesson09/new/spy.py
@@ -4,7 +4,7 @@ import datetime
 
 
 def log(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    path = 'G:/Учеба/Разработчик/repo/Python/HomeWork/Lesson09/new/log.csv'
+    path = 'G:/Учеба/Разработчик/repo/Python/HomeWork/Lesson09/new/log1.csv'
     file = open(path, 'a', encoding='utf-8')
     file.write(f'{datetime.datetime.now().time()};{update.effective_user.first_name}; {update.effective_user.id}; {update.message.text}\n')
     file.close()
